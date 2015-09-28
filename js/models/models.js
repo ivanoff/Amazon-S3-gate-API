cbcd86f285d05ee3c33915baef8e50f5e95b4ca6
d-i-m-a <my.name.is.dimitry@gmail.com>
2015-09-28 12:34:37 +0300

    more readeble

--- js/models/models.js
+++ js/models/models.js
@@ -1,48 +1,32 @@
 
 // All types defenition
 var Types = {
-    // uuid methods
-    uuid : {
-        // uuid.generate returns new UUID 
-        generate : function(){
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                return v.toString(16);
-            });
-        },
+    uuid : { // uuid methods
         // uuid.check returns true if parameter looks like UUID, false otherwise 
         check : function( uuid ){
-            return uuid && uuid.match(/^[\da-z]{8}-[\da-z]{4}-4[\da-z]{3}-[\da-z]{4}-[\da-z]{12}$/)?;
+            return uuid && uuid.match(/^[\da-z]{8}-[\da-z]{4}-4[\da-z]{3}-[\da-z]{4}-[\da-z]{12}$/);
         },
     },
-    // string properties and methods
-    string : {
-        // string.min Minimum length of the string
-        min   : 0,
-        // string.max Maximum length of the string
-        max   : Infinity,
-        // string.check check sting type and size
-        check : function( string ){
+    string : { // string properties and methods
+        min   : 0,        // string.min Minimum length of the string
+        max   : Infinity, // string.max Maximum length of the string
+        check : function( string ){ // string.check check sting type and size
             return (( typeof string === 'string' || string instanceof String )
                     && string.length >= this.min 
                     && string.length <= this.max );
         },
     },
-    // number properties and methods
-    number : {
-        // number.min Minimum number value
-        min   : -Infinity,
-        // number.max Maximum number value
-        max   : Infinity,
-        // number.check check number type and size
-        check : function( number ){
-            return typeof number === 'number' && number >= this.min && number <= this.max;
+    number : { // number properties and methods
+        min   : -Infinity,   // number.min Minimum number value
+        max   : Infinity,    // number.max Maximum number value
+        check : function( number ){ // number.check check number type and size
+            return typeof number === 'number' 
+                    && number >= this.min 
+                    && number <= this.max;
         },
     },
-    // date methods
-    date : {
-        // date.check Maximum length of the string
-        check : function( date ){
+    date : { // date methods
+        check : function( date ){  // date.check Maximum length of the string
             return date instanceof Date && typeof date.getMonth === 'function';
         },
     },
