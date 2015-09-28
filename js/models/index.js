bd81f9fa3feb2f57e76943d0bfc80cb1e798eb86
d-i-m-a <my.name.is.dimitry@gmail.com>
2015-09-28 12:50:36 +0300

    remove examples

--- js/models/index.js
+++ js/models/index.js
@@ -39,46 +39,6 @@ myLibrary.consoleTrueOrError ( myLibrary.validate( "user", { name  : "Alex Barda
 console.log( 'Check 4:' );
 myLibrary.consoleTrueOrError ( myLibrary.validate( "user", { id : "1cecfb4-da43-4b65-aaa0-f1c3be81ec53", imya : "Alex Bardanov" }) );
 
-/*// Password example
-myLibrary.registerModel( "user3", {
-  id:   { type: "uuid", required: true },                        // property “id” must be uuid
-  name: { type: "string", min: 4, max: 128, required: true },    // property “name” must be String and contain 4-128
-  name: { type: "password", min: 4, required: true  },         // property “password” must be String and contain not less than 4 characters
-} );
-*/
-
-/*// Exceptions examples
-/// Required field key not found exception
-myLibrary.consoleTrueOrError ( myLibrary.validate( "user", { name  : "Alex Bardanov" }) );
-
-/// 2 errors: Field not matched with type exception and Field not found
-myLibrary.consoleTrueOrError ( myLibrary.validate( "user", { id : "1cecfb4-da43-4b65-aaa0-f1c3be81ec53", imya : "Alex Bardanov" }) );
-
-/// Size minimum check error
-myLibrary.consoleTrueOrError ( myLibrary.validate( "user", { id : "61cecfb4-da43-4b65-aaa0-f1c3be81ec53", name : "" }) );
-
-/// Size maximum check error
-myLibrary.consoleTrueOrError ( myLibrary.validate( "user", { id : "61cecfb4-da43-4b65-aaa0-f1c3be81ec53", name : "ASNKJW oew  owek rewRWIWJG OERGMLkf gsojejrwoeg ke r gerEGIOJWgij i4 ggr" }) );
-
-/// undefined username exception
-myLibrary.registerModel( undefined, { id: { type: "uuid", required: true } } );
-
-// undefined model exception
-myLibrary.registerModel( "name_exception", NaN );
-
-/// name duplicate exception
-myLibrary.registerModel( "user", { id: { type: "uuid", required: true } } );
-
-// no type field exception
-myLibrary.registerModel( "name_exception", { date: { parameter: "date" } } );
-
-// no guid type exception
-myLibrary.registerModel( "name_exception", { id: { type: "guid" } } );
-
-// minimum value less than required exception
-myLibrary.registerModel( "name_exception", { name: { type: "string", min: -11, max: 64 } } );
-*/
-
 
 myLibrary.dispose();
 
