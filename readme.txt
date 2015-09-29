513f9b36f6c1fe4a6ef3336d8611947ec8ad0e19
d-i-m-a <my.name.is.dimitry@gmail.com>
2015-09-29 12:45:12 +0300

    release of web_server_express

--- readme.txt
+++ readme.txt
@@ -1,10 +1,161 @@
+Users manager
 
-First run
-Please run npm to install all necessary dependencies
+What Is This?
+-------------
+Simple web server using NodeJS express and MongoDB to manage of users
+
+
+Install
+-------------
+1. Unpack all files from archive and cd to folder
+    tar zxf web_server_express.tar.gz
+    cd web_server_express
+
+2. Run npm to install all necessary dependencies
     npm install
 
-npm install express --save
-npm install mongodb --save
-npm install node-uuid --save
-npm install config --save
-npm install body-parser --save
+3. Edit config file to change parameters
+    vi config/default.json
+
+Usage
+-------------
+To launch web server please use command below
+    node web_server_express.js
+
+Get list of all users
+    curl 127.0.0.1:3000/users
+
+Add new user
+    curl -d "first_name=Max&last_name=Abramsky&email=aa@aa.ua" 127.0.0.1:3000/users
+
+Get user by id
+    curl 127.0.0.1:3000/users/4c5adf87-c7be-47ec-a946-be23d5527e49
+
+Update user by id ( patch )
+    curl -d "first_name=Maxim&email=max@abramsky.com" 127.0.0.1:3000/users/4c5adf87-c7be-47ec-a946-be23d5527e49
+
+Remove user
+    curl -X "DELETE" 127.0.0.1:3000/users/3931b794-1aef-4520-bd39-236017599a4a
+
+
+Additional modules
+-------------
+All additional modules are located in ./modules folder.
+Module db.js is necessary for work with MongoDB database
+Modules models.js and types.js are necessary for validate incoming data ( planned )
+
+
+Errors
+-------------
+There is a count of errors that can be encountered. All these errors are in config file.
+    101 - First name not found
+    102 - Last name not found
+    103 - Email field not found
+    111 - first_name don't match (contains special chars)
+    112 - last_name don't match (contains special chars)
+    113 - email field don't match
+    121 - User not found
+    201 - Insert was fail
+    201 - Update was fail
+
+
+List of NodeJS dependencies
+-------------
+    express 
+    mongodb 
+    node-uuid 
+    config 
+    body-parser 
+
+
+Output example
+-------------
+$ node -v
+    v0.10.40
+
+curl 127.0.0.1:3000/users
+    No users found. Please, use POST to add new user.
+
+curl -d "first_name=Max&last_name=Abramsky&email=aa@aa.ua" 127.0.0.1:3000/users
+    Recors was inserted. New record id: 4c5adf87-c7be-47ec-a946-be23d5527e49
+
+curl 127.0.0.1:3000/users/4c5adf87-c7be-47ec-a946-be23d5527e49
+      ID|  Last name|  First name|  Email
+    4c5adf87-c7be-47ec-a946-be23d5527e49|  Abramsky|  Max|  aa@aa.ua|  [object Object]
+
+curl -d "first_name=Linda&last_name=Abramsky&email=aa@aa.ua" 127.0.0.1:3000/users
+    Recors was inserted. New record id: 3931b794-1aef-4520-bd39-236017599a4a
+
+curl 127.0.0.1:3000/users/3931b794-1aef-4520-bd39-236017599a4a
+      ID|  Last name|  First name|  Email
+    3931b794-1aef-4520-bd39-236017599a4a|  Abramsky|  Linda|  aa@aa.ua|  [object Object]
+
+curl 127.0.0.1:3000/users
+      ID|  Last name|  First name|  Email
+    4c5adf87-c7be-47ec-a946-be23d5527e49|  Abramsky|  Max|  aa@aa.ua|  [object Object]
+    3931b794-1aef-4520-bd39-236017599a4a|  Abramsky|  Linda|  aa@aa.ua|  [object Object]$ 
+
+curl -d "last_name=Belovich" 127.0.0.1:3000/users/3931b794-1aef-4520-bd39-236017599a4a
+    Recors 3931b794-1aef-4520-bd39-236017599a4a was updated
+
+curl 127.0.0.1:3000/users/
+      ID|  Last name|  First name|  Email
+    4c5adf87-c7be-47ec-a946-be23d5527e49|  Abramsky|  Max|  max@abramsky.com|  
+    3931b794-1aef-4520-bd39-236017599a4a|  Belovich|  Linda|  aa@aa.ua|  [object Object]
+
+curl -d "first_name=Maxim&email=max@abramsky.com" 127.0.0.1:3000/users/4c5adf87-c7be-47ec-a946-be23d5527e49
+    Recors 4c5adf87-c7be-47ec-a946-be23d5527e49 was updated
+
+curl 127.0.0.1:3000/users
+      ID|  Last name|  First name|  Email
+    4c5adf87-c7be-47ec-a946-be23d5527e49|  Abramsky|  Maxim|  max@abramsky.com|  
+    3931b794-1aef-4520-bd39-236017599a4a|  Belovich|  Linda|  aa@aa.ua|  [object Object]
+
+curl -X "DELETE" 127.0.0.1:3000/users/3931b794-1aef-4520-bd39-236017599a4a
+    User 3931b794-1aef-4520-bd39-236017599a4a removed
+
+curl 127.0.0.1:3000/users
+      ID|  Last name|  First name|  Email
+    4c5adf87-c7be-47ec-a946-be23d5527e49|  Abramsky|  Maxim|  max@abramsky.com|  
+
+
+Errors Examples
+-------------
+curl -d "first_name=Maxim&email=max@abramsky.com" 127.0.0.1:3000/users
+    Error 400: Post syntax incorrect. #102: Last name not found 
+
+curl -d "last_name=df*a&first_name=Maxim&email=max@abramsky.com" 127.0.0.1:3000/users
+    Error 400: Post syntax incorrect. #112: last_name don't match (contains special chars) 
+
+curl -d "last_name=Saltikov-Shedrin&first_name=Max\`D\`Art&email=max-abramsky.com" 127.0.0.1:3000/users
+    Error 400: Post syntax incorrect. #113: email field don't match 
+
+curl -d "last_name=Saltikov-Shedrin&first_name=Max\`D\`Art&email=max@abramsky.com" 127.0.0.1:3000/users
+    Recors was inserted. New record id: 4804aa7b-c7c4-47d3-beb0-d08220ac175b 
+
+curl 127.0.0.1:3000/users/4804aa7b-c7c4-47d3-beb0
+    Error 404: Not found. #121: User not found 
+
+curl -d "last_name=d)" 127.0.0.1:3000/users/4804aa7b-c7c4-47d3-beb0
+    Error 400: Post syntax incorrect. #112: last_name don't match (contains special chars) 
+ 
+curl -d "last_name=dd" 127.0.0.1:3000/users/4804aa7b-c7c4-47d3-beb0
+    Error 404: Not found. #121: User not found 
+
+
+File list
+-------------
+web_server_express.js   web script to launch
+config/default.json     config file
+modules/db.js     work with MongoDB database
+modules/models.js validator module
+modules/types.js  types to validate
+package.json      package information
+readme.txt        this readme
+
+
+Created by
+-------------
+Dimitry, 2@ivanoff.org.ua
+curl -A cv ivanoff.org.ua
+
--- web_server_express.js
+++ web_server_express.js
@@ -1,20 +1,29 @@
+/****
+ Simple web server
+ Usage:
+    node web_server_express.js
+ Created by:
+    Dimitry, 2@ivanoff.org.ua
+****/
 
 var config  = require( 'config'    );
 var express = require( 'express'   );
 var uuid    = require( 'node-uuid' );
-
 var bodyParser = require('body-parser');
 
-var db = require( './db' );
+var db = require( './modules/db' );
+var models = require( './modules/models' );
 
 var app = express();
 app.use( bodyParser.urlencoded({ extended: true }) );
 
+// params from config file ( ./config/default.json )
 var URL        = config.get( 'DB.url' ),
     COLLECTION = config.get( 'DB.collection' ),
     PORT       = config.get( 'SERVER.port' ),
     ERROR      = config.get( 'ERROR' );
 
+// short template to show list of users
 showUsers = function ( users ) {
     return "  ID\t\t\t\t\t|  Last name\t|  First name\t|  Email\n" + 
     users
@@ -25,6 +34,7 @@ showUsers = function ( users ) {
 }
 
 
+// Connect to DB and start express
 db.connect( URL, function( err ) {
     if ( err ) {
         console.log( 'Unable to connect to Mongo' );
@@ -40,7 +50,13 @@ db.connect( URL, function( err ) {
 
 app.get('/', function (req, res) { res.redirect('/users') })
 
-
+// GET all users from DB
+// Example:
+//   curl 127.0.0.1:3000/users
+// Example output:
+//     ID|  Last name|  First name|  Email
+//     4c5adf87-c7be-47ec-a946-be23d5527e49|  Abramsky|  Max|  aa@aa.ua|
+//     3931b794-1aef-4520-bd39-236017599a4a|  Abramsky|  Linda|  aa@aa.ua|
 app.get('/users', function (req, res) {
     db.collection.find().toArray(function( err, users ) {
         if ( users[0] ) {
@@ -51,7 +67,12 @@ app.get('/users', function (req, res) {
     })
 })
 
-
+// GET user from DB. Return 404 if user not found
+// Example:
+//   curl 127.0.0.1:3000/users/3931b794-1aef-4520-bd39-236017599a4a
+// Example output:
+//     ID       |  Last name    |  First name   |  Email
+//     3931b794-1aef-4520-bd39-236017599a4a|  Abramsky|  Linda|  aa@aa.ua| 
 app.get('/users/:id', function (req, res) {
     db.collection.find( { _id : req.params.id } ).toArray(function( err, users ) {
         if ( users[0] ) {
@@ -64,17 +85,21 @@ app.get('/users/:id', function (req, res) {
     })
 })
 
-
+// POST users to DB
+// Example:
+//   curl -d "first_name=Linda&last_name=Abramsky&email=aa@aa.ua" 127.0.0.1:3000/users
+// Example output:
+//   Recors was inserted. New record id: 3931b794-1aef-4520-bd39-236017599a4a
 app.post('/users', function (req, res) {
 
     // check for errors
     var error;
 
     if( !req.body.first_name ) error = 101
-    else if( !req.body.first_name.match(/^[\w\ _]+$/i) ) error = 111;
+    else if( !req.body.first_name.match(/^[\w\ _`\-]+$/i) ) error = 111;
 
     if( !req.body.last_name ) error = 102
-    else if( !req.body.last_name.match(/^[\w\ _]+$/i) ) error = 112;
+    else if( !req.body.last_name.match(/^[\w\ _`\-]+$/i) ) error = 112;
 
     if( !req.body.email ) error = 103
     else if( !req.body.email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+/i) ) error = 113;
@@ -84,6 +109,7 @@ app.post('/users', function (req, res) {
         return res.send('Error 400: Post syntax incorrect. #' + error + ": " + ERROR[error] );
     } 
 
+    // make structure to add to the DB
     var id = uuid.v4()
     var data = {
         _id: id,
@@ -107,50 +133,65 @@ app.post('/users', function (req, res) {
 
 })
 
-
+// PATCH user by :id. Return 404 if user not found
+// Example:
+//   curl -d "first_name=Maxim&last_name=Abramsky&email=max@abramsky.com" 127.0.0.1:3000/users/dceb2717-34ce-4bd4-92ac-893e65bb1504
+// Example output:
+//   Recors dceb2717-34ce-4bd4-92ac-893e65bb1504 was updated
 app.post('/users/:id', function (req, res) {
 
     // check for errors
     var error;
 
-    if( !req.body.first_name ) error = 101
-    else if( !req.body.first_name.match(/^[\w\ _]+$/i) ) error = 111;
+    // try to find record to update
+    db.collection.findOne( { _id : req.params.id }, function( err, data ) { 
+        if ( !data ) {
+            res.statusCode = 404;
+            error = 121;
+            return res.send('Error 404: Not found. #' + error + ": " + ERROR[error] );
+        } else {
 
-    if( !req.body.last_name ) error = 102
-    else if( !req.body.last_name.match(/^[\w\ _]+$/i) ) error = 112;
+            // check incoming parameters
+            if( req.body.first_name ) {
+                if( !req.body.first_name.match( /^[\w\ _`\-]+$/i) ) { error = 111 } 
+                else { data.name.first = req.body.first_name };
+            }
+            if( req.body.last_name ) {
+                if( !req.body.last_name.match ( /^[\w\ _`\-]+$/i) ) { error = 112 } 
+                else { data.name.last = req.body.last_name };
+            }
+            if( req.body.email ) {
+                if( !req.body.email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+/i) ) { error = 113 } else { data.email = req.body.email };
+            }
+            if ( req.body.metadata ) data.metadata = req.body.metadata;
+        
+            if ( error ) {
+                res.statusCode = 400;
+                return res.send('Error 400: Post syntax incorrect. #' + error + ": " + ERROR[error] );
+            } 
 
-    if( !req.body.email ) error = 103
-    else if( !req.body.email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+/i) ) error = 113;
-    
-    if ( error ) {
-        res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect. #' + error + ": " + ERROR[error] );
-    } 
+            // try update record
+            db.collection.update( { _id : req.params.id }, data, function(err, result) {
+                if (err) {
+                    res.statusCode = 500;
+                    error = 202;
+                    return res.send('Error 500: Internal Server Error. #' + error + ": " + ERROR[error] + ' : ' + err );
+                 } else {
+                    return res.send('Recors ' + req.params.id + ' was updated' );
+                }
+            } )
 
-    var data = {
-        name: {
-            first : req.body.first_name,
-            last  : req.body.last_name,
-        },
-        email: req.body.email,
-        metadata: req.body.metadata? req.body.metadata : {},
-    }
-
-    db.collection.update( { _id : req.params.id }, data, function(err, result) {
-        if (err) {
-            res.statusCode = 500;
-            error = 202;
-            return res.send('Error 500: Internal Server Error. #' + error + ": " + ERROR[error] + ' : ' + err );
-         } else {
-            return res.send('Recors ' + req.params.id + ' was updated' );
         }
-    } )
+    });
+
 
 })
 
-// Delete user from BD
+// DELETE user from BD
 // Example:
-//   curl -X "DELETE" 127.0.0.1:3000/users/fc2d9b8f-4569-437c-b29f-5d2724c12349
+//   curl -X "DELETE" 127.0.0.1:3000/users/3931b794-1aef-4520-bd39-236017599a4a
+// Example output:
+//   User 3931b794-1aef-4520-bd39-236017599a4a removed
 app.delete('/users/:id', function (req, res) {
     db.collection.remove( { _id : req.params.id } ,function( err, user ) {
         if (err) {
