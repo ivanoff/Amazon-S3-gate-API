28379b07eeadb7cc6108b749c5a2e94f22a01604
d-i-m-a <my.name.is.dimitry@gmail.com>
2015-10-15 22:56:47 +0300

    try to test

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
-"use strict"
+var server = require('./server' );
 
-var config = require('config');
-var express = require('express');
-var uuid = require( 'node-uuid' );
-var bodyParser = require('body-parser');
-var mongo = require('mongodb');
-var bunyan = require('bunyan');
-var _ = require('underscore');
+server.start( function(){
+//setTimeout( function(){console.log('3 seconds fall');server.stop();}, 3000);
+} );
 
-var DB_URL     = process.env.DB_URL,
-    PORT       = process.env.SERVER_PORT,
-    LOG_PATH   = process.env.LOG_PATH,
-    ERRORS     = config.get( 'ERRORS' );
-
-var app = new express();
-
-var db = require('./controllers/db');
-
-var log = bunyan.createLogger( { 
-  name: "server",
-  port: PORT,
-  streams: [
-      { level: 'debug', stream: process.stdout },
-      { level: 'error', path: LOG_PATH+'/error.log' }
-  ]
-});
-
-app.use( function( req, res, next ){ 
-    req._     = _; 
-    req.db    = db.get(); 
-    req.log   = log; 
-    req.uuid  = uuid; 
-    req.warn  = function(n,e){ 
-                    req.log.warn( { warn : n } , e);
-                }; 
-    req.error = function(n,e){ 
-                    var text=ERRORS[e] || e; 
-                    if( text==e ) e=1; 
-                    req.log.error( { error : e } , text);
-                    res.status(n).json( {error:e, text:text} )
-                }; 
-    next() 
-});
-app.use( bodyParser.json() );
-
-var normalizedPath = require("path").join(__dirname, "routes");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./routes/" + file)(app);
-});
-
-db.connect( DB_URL, function( err, next ) {
-    if ( err ) { return next( err ) }
-    app.listen( PORT, function() {
-        log.info({pid:1},'['+PORT+'] server started');
-        console.log( 'Listening on port ' + PORT + '...' )
-    })
-})
 
---
+++ start_test.sh
@@ -0,0 +1,6 @@
+
+export DB_URL=mongodb://gl:gl@ds051933.mongolab.com:51933/gl
+export SERVER_PORT=3000
+export LOG_PATH=./log
+
+make test
