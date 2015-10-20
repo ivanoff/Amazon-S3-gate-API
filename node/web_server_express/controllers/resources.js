227c96315c9e4a13614d2e916fb1e434ec9f3c19
d-i-m-a <my.name.is.dimitry@gmail.com>
2015-10-20 09:52:54 +0300

    update search resources by type

--- node/web_server_express/controllers/resources.js
+++ node/web_server_express/controllers/resources.js
@@ -21,18 +21,10 @@ exports.getResourcesTotal = function( req, res, next ){
 };
 
 exports.getResourcesByType = function( req, res, next ) {
-    ResourcesModel.get( req, function( err, doc ){
-        if ( err  ) { req.error( 500, err ); return next(err) }
-        if ( !doc ) { req.error( 404, ERROR.ASSET_NOT_FOUND ); return next() }
-        if ( doc.type != 'folder' ) {
-            res.json( doc );
-        } else {
-            ResourcesModel.getFolderContent( req, doc.path + '/' + doc.name, function( err, doc ){
-                if ( err  ) { req.error( 500, err ); return next(err) }
-                if ( !doc ) { req.error( 404, ERROR.ASSET_NOT_FOUND ); return next() }
-                res.json( doc );
-            });
-        }
+    ResourcesModel.getResourcesByType( req, function( err, docs ){
+        if ( err   ) { req.error( 500, err ); return next(err) }
+        if ( !docs ) { req.error( 404, ERROR.NO_ASSETS ); return next() }
+        res.json( docs );
     });
 };
 
